638f2b1808c3d07064a95dca98cf42e7ba0df0ae
Jogius <jogius.julius.makowski@gmail.com>
2020-10-21 12:56:39 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, {createRef} from 'react';
+import React, {useState} from 'react';
 import Axios from 'axios';
 
 import './App.css';
-const fileInput = createRef();
-const textInput = createRef();
 
 function App() {
+  const [files, setFiles] = useState(null);
+  const [text, setText] = useState("");
+
   function handleSubmit(e) {
     e.preventDefault();
     const formData = new FormData();
-    formData.append("text", textInput.current.value);
-    formData.append("files", fileInput.current.files);
+    formData.append("text", text);
+    formData.append("files", files);
     Axios.post(
       "http://thomklauke.ddns.net:3000/print",
       formData,
@@ -33,9 +34,9 @@ function App() {
         <legend className="legend">Drucken</legend>
         <form className="form">
           <label className="label" htmlFor="file">Dateien</label>
-          <input className="formelement" type="file" id="file" name="file" multiple ref={fileInput} />
+          <input className="formelement" type="file" id="file" name="file" multiple onChange={(e) => setFiles(e.target.files)} />
           <label className="label" htmlFor="text">Text</label>
-          <textarea className="formelement" id="text" name="text" placeholder="Text hier eingeben" ref={textInput} />
+          <textarea className="formelement" id="text" name="text" placeholder="Text hier eingeben" value={text} onChange={(e) => setText(e.target.value)} />
           <button className="submit" type="submit" id="submit" name="submit" onClick={handleSubmit}>
             Submit
           </button>
